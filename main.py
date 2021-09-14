4ba9684c7295f1a9ae8461b7c07ee44797873bfb
Admirim Korici <admirim_korici.fshnstudent@unitir.edu.al>
2021-09-14 23:23:19 +0200

    Changes

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from bs4 import BeautifulSoup
 html_text = requests.get('https://www.900gpa.com/en/search?p=resin&u=metric&refinements%5BproductInformation.matrixType%5D%5B0%5D=Thermoset&refinements%5BproductInformation.resinType%5D%5B0%5D=BMI&uid=ResinPreg_00EDB4EC83').text
 soup = BeautifulSoup(html_text, 'lxml')
 
-tables = soup.find()
+tables = soup.find_all()
 print(tables)
