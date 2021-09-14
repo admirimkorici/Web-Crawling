2b4dd881e9ce6b30c254428dda38fb1edfb3b24a
Admirim Korici <admirim_korici.fshnstudent@unitir.edu.al>
2021-09-14 23:22:09 +0200

    Changes

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
-html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=Python&txtLocation=').text
-
+html_text = requests.get('https://www.900gpa.com/en/search?p=resin&u=metric&refinements%5BproductInformation.matrixType%5D%5B0%5D=Thermoset&refinements%5BproductInformation.resinType%5D%5B0%5D=BMI&uid=ResinPreg_00EDB4EC83').text
 soup = BeautifulSoup(html_text, 'lxml')
 
-tables = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+tables = soup.find()
 print(tables)
