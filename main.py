c5f0d452eeb67dc05a94a93d87a89656a0003695
Admirim Korici <admirim_korici.fshnstudent@unitir.edu.al>
2021-09-17 13:16:04 +0200

    Changes

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-import requests
+from selenium import webdriver
 from bs4 import BeautifulSoup
-html_text = requests.get('https://www.900gpa.com/sitemap.xml').text
-soup = BeautifulSoup(html_text, 'lxml')
 
-tables = soup.find_all('url')
-print(tables)
+driver = webdriver.Chrome()
+url = 'https://www.900gpa.com/en/search?p=resin&u=metric&refinements%5BproductInformation.matrixType%5D%5B0%5D=Thermoset&refinements%5BproductInformation.resinType%5D%5B0%5D=BMI&uid=ResinPreg_00EDB4EC83'
+driver.get(url)
+html = driver.page_source
+soup = BeautifulSoup(html, 'lxml')
+content = soup.find_all('table')
+
+for con in content:
+    title = con.h4.text
+    print(title)
