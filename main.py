458b320366919ffecbd7d1cf4e9f17ca0310e80c
Admirim Korici <admirim_korici.fshnstudent@unitir.edu.al>
2021-09-18 08:30:02 +0200

    Changes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ driver = webdriver.Chrome()
 url = 'https://www.900gpa.com/en/search?p=resin&u=metric&refinements%5BproductInformation.matrixType%5D%5B0%5D=Thermoset&refinements%5BproductInformation.resinType%5D%5B0%5D=BMI&uid=ResinPreg_00EDB4EC83'
 driver.get(url)
 html = driver.page_source
-soup = BeautifulSoup(html, 'lxml')
+soup = BeautifulSoup(html, 'html.parser')
 content = soup.find_all('table')
 
 dictionary = dict()
